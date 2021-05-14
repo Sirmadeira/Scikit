39e74a7d3aef60bd8491629d5a65dbce20b8e563
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-05-14 16:16:17 -0300

    Update Intro scikit.py

--- Aula1/Intro scikit.py
+++ Aula1/Intro scikit.py
@@ -25,8 +25,8 @@ print(len(digits.data))
 x,y = digits.data[:-10], digits.target[:-10]
 clf.fit(x,y)
 # Isso daki seria basicamente a tentativa de interpretar o numero dado nesse caso esto dando o antes do penultimo
-print("Predicao do numero dado:",clf.predict(digits.data[[-6]]))
+print("Predicao do numero dado:",clf.predict(digits.data[[-5]]))
 #Cmap e interpolation e para disponibilizar o datase de maneira visual, interpolation pega os mais pertos e destaca eles com certa cor e cmap,
 # colore tudo e deixa um pouquinho com menos zomm
-plt.imshow(digits.images[-6], cmap= plt.cm.gray_r, interpolation= 'nearest')
+plt.imshow(digits.images[-5], cmap= plt.cm.gray_r, interpolation= 'nearest')
 plt.show()
